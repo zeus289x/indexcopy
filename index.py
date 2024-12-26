34cf0a5c3bc75a5deb6e39b70baf104e6b540376
zeus289x <mertakca1a@gmail.com>
2024-12-26 22:32:26 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import os
 import webbrowser
 import requests
+import sys
+
+downloads_folder = '/storage/emulated/0/Download' 
 
 os.system("clear")
 
@@ -15,7 +18,7 @@ print("\033[1;36mBu tool Zeus289 tarafından hazırlanmıştır.\033[0m")
 print("\033[1;34mDiscord: https://discord.gg/289\033[0m")
 print("")
 
-url = input("Hedef URL (örnek: https://example.com): ")
+url = input("Hedef URL örnek: https://example.com: ")
 
 if not url:
     print("Bir URL girmelisin!")
@@ -26,9 +29,10 @@ print("Sitenin indexi indiriliyor...")
 try:
     response = requests.get(url)
     response.raise_for_status()
-    with open("index.html", "w", encoding="utf-8") as file:
+    file_path = os.path.join(downloads_folder, "index.html")
+    with open(file_path, "w", encoding="utf-8") as file:
         file.write(response.text)
-    print("\033[1;32mIndex başarıyla indirildi! 'index.html' dosyasına kaydedildi.\033[0m")
+    print(f"\033[1;32mIndex başarıyla indirildi! '{file_path}' dosyasına kaydedildi.\033[0m")
     print("\033[1;33mDiscord linkine yönlendiriliyorsunuz...\033[0m")
     webbrowser.open("https://discord.gg/289")
 except Exception as e:
